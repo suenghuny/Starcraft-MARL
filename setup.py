2c07ef62ba8e546bf16d63d882b29a76d7d8a3d9
suenghuny <61965404+suenghuny@users.noreply.github.com>
2023-01-25 17:28:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import find_packages, setup
 setup(
    name='Starcraft MARL',
    version='1.0',
-   packages=find_packages(),  # would be the same as name
+   packages=['GAT', 'GTN'],  # would be the same as name
    )
