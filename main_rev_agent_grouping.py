bb12873e21334153477d47db4beadf1bba0d90f0
suenghuny <61965404+suenghuny@users.noreply.github.com>
2023-01-27 09:40:36 +0900

    Update main_rev_agent_grouping.py

--- main_rev_agent_grouping.py
+++ main_rev_agent_grouping.py
@@ -1,7 +1,22 @@
 from smac_rev import StarCraft2Env
 from GDN import Agent
 import torch
-#from torch.utils.tensorboard import SummaryWriter
+from pysc2.lib.remote_controller import ConnectError, RequestError
+from pysc2.lib.protocol import ProtocolError
+from functools import partial
+import sys
+import os
+import vessl
+
+vessl.init()
+REGISTRY = {}
+REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+if sys.platform == "linux":
+    os.environ.setdefault("SC2PATH",
+                          os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
+
+def env_fn(env, **kwargs):
+    return env(**kwargs)
 
 regularizer = 0.0
 map_name1 = '3s5z'
@@ -158,9 +173,7 @@ def train(agent, env, e, t, train_start, epsilon, min_epsilon, anneal_epsilon, i
 
 def main():
     #writer = SummaryWriter('logs/')
-    env1 = StarCraft2Env(map_name=map_name1, step_mul=8, replay_dir="Replays", seed=123)
-
-
+    env1 = REGISTRY["sc2"](map_name=map_name1, seed=123, step_mul=8, replay_dir="Replays", )
     env1.reset()
     num_unit_types, unit_type_ids = get_agent_type_of_envs([env1])
     env1.generate_num_unit_types(num_unit_types, unit_type_ids)
